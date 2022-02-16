413071fbeeb15109c41dc8571939b168591a8a28
Natalia Naranjo <90767304+natynaro@users.noreply.github.com>
2022-02-16 16:37:20 -0500

    trabajo1

---
+++ src/Trabajo1/Programar.java
@@ -0,0 +1,43 @@
+package Trabajo1;
+
+public class Programar {
+	
+	
+	/*
+	 * - pedidosTotal: Pedidos[]
+	 * - ingredientesTotal :Ingredientes[]
+	 * - platosTotal: Platos[]
+	 * - domiciliarios: Domiciliarios[]
+	 * 
+	 * 
+	 * + ModificarPrecioPlato(String, double)
+	 * + ModificarIngrePlato(String, Ingredientes[])
+	 * + NuevoPedido(String[], String, String, String)
+	 * 
+	 * public void ModificarPrecioPlato(String plato, double precio){
+	 * 
+	 * int i=0
+	 *
+	 * while(i<platosTotal.lenght && !platosTotal[i].getNombre.equals(plato))i++;
+	 * 
+	 * if(i<platosTotal.lenght){
+	 * 	platosTotal[i].setPrecio(precio);
+	 * }else {//un throw??}
+	 * 
+	 * 
+	 * 
+	 * 
+	 * }
+	 * 
+	 * public void ModificarIngrePlato(String plato, Ingredientes[] nuevosIngre){
+	 * 
+	 * 
+	 * }
+	 * 
+	 * public void NuevoPedido(String[] nombresPlatos, String nombreUsu, String direccionUsu, String telefonoUsu){
+	 * }
+	 * 
+	 * 
+	 * 
+	 * */
+}
