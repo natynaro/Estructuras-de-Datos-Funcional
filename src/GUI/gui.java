0d07d8e3519bd491850aac2866f7a0dc75960f4e
Natalia Naranjo <90767304+natynaro@users.noreply.github.com>
2022-02-23 18:33:50 -0500

    Create gui.java

---
+++ src/GUI/gui.java
@@ -0,0 +1,17 @@
+package GUI;
+//import javax.swing.*;
+
+public class gui {
+
+	public static void main(String[] args) {
+		/*// TODO Auto-generated method stub
+		JFrame frame = new JFrame("Mi primera GUI");      
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
+        frame.setSize(300, 300);      
+        Botón JButton = nuevo JButton("Presionar");      
+        frame.getContentPane().add(botón); // Agrega el botón al panel de contenido del marco     
+        frame.setVisible(true);  */
+
+	}
+
+}
