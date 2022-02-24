7138c53946f05590bf3f4e1c6dedc988a96ff023
Natalia Naranjo <90767304+natynaro@users.noreply.github.com>
2022-02-24 11:46:56 -0500

    ,

--- src/Ejercicio2402/mainNum.java
+++ src/Ejercicio2402/mainNum.java
@@ -4,9 +4,9 @@ public class mainNum {
 
 	public static void main(String[] args) {
 		int[] numm= {-2, 11, -4, 13, -5, 2};
-		System.out.println(num.mayor(numm));
-		//System.out.println(num.mayor2(numm));
-		System.out.println(num.subsecuenciaSumaMaxima(numm));
+		//System.out.println(num.mayor(numm));
+		System.out.println(num.mayor2(numm));
+		//System.out.println(num.subsecuenciaSumaMaxima(numm));
 		
 		
 	}
--- src/Ejercicio2402/num.java
+++ src/Ejercicio2402/num.java
@@ -9,50 +9,56 @@ public class num {
 		int sumaMayor=0;//2
 		
 		
-		for(int i=0; i<array.length; i++) {//2+n+
-			sumaActual=array[i];
-			for(int j=i+1;j<array.length; j++) {
-				sumaActual+=array[j];
-				if(sumaActual>sumaMayor) {
-					sumaMayor=sumaActual;
+		for(int i=0; i<array.length; i++) {//2n+3
+			sumaActual=array[i];//1
+			for(int j=i+1;j<array.length; j++) {//n^2+4n+3
+				sumaActual+=array[j];//n^2-n
+				if(sumaActual>sumaMayor) {//n^2-n/2
+					sumaMayor=sumaActual;//n^2-n/2
 				}
 			}
 			
 		}
-		return sumaMayor;
-	}
-	/*
+		return sumaMayor;//1
+	}//O(3N^2+6N+11)=O(N^2)
+	
+	
 	public static int mayor2(int[] array) {
-		int sumaMayor=0;//2
 		
-		for(int i=0, j=i+1, sumaActual=array[i]; j<array.length; i++, j++) {//2+n+
-			do {
-				sumaMayor=sumaActual;
-				sumaActual+=array[j];
-				j++;
-			} while(sumaMayor<sumaActual && j<array.length);
+		int  j=1, sumaActual=array[0], sumaMayor=0;//8
+		for(int i=0; j<array.length; i++ ){
+			if(sumaMayor<sumaActual) {
+				do{
+					sumaMayor=sumaActual;
+					sumaActual+=array[j];
+					j++;
+				}while(sumaMayor<sumaActual);
+			}
 			
 		}
+		
+		
 		return sumaMayor;
-	}*/
+	}
 	
 	public static int subsecuenciaSumaMaxima (int [] a)
 	{
 	 int sumaActual; //1
 	 int sumaMax = 0;//2
 	 for ( int i =0; i<a.length; i++)//2n+3
-	 for ( int j=i; j<a.length; j++)//n(2+n-i+1+n-i)=n(2n+2-2i)
-	 {
-	 sumaActual = 0;//1
-	 for ( int k=i; k<=j; k++)
-	 sumaActual += a[k];
-	 if (sumaActual > sumaMax)
-	 {
-	 sumaMax = sumaActual;
-	 int secIni = i;
-	 int secFin = j;
-	 }
-	 }
-	 return sumaMax;
+		 for ( int j=i; j<a.length; j++){//
+			 
+			 sumaActual = 0;//
+			 for ( int k=i; k<=j; k++)//
+				 sumaActual += a[k];//(n^3/3)+(n^2)+(2n/3)
+			 
+			 if (sumaActual > sumaMax){//
+				 
+				 sumaMax = sumaActual;//
+				 int secIni = i;//
+				 int secFin = j;//
+			 }
+		 }
+	 	return sumaMax;//1
 	 }
 }
